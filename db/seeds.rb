452268696c3996123aea34062619572145961f51
Jeroen Houben <jeroen@supercool.nl>
2015-12-21 14:51:34 +0100

    Add some products and an overview

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create([{ name: 'Pen', description: 'A beautiful green pen. Great for writing!' }, 
+                { name: 'BMW 525i', description: 'Fast German made car. Impress your friends!' }])
